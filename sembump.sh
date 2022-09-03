f0aad74bccf76f105a77ac4f43cc5fc68511f7af
i5irin <contact@i5ir.in>
2022-09-03 09:56:13 +0900

    feat: add the ability to version up from the update history

--- sembump.sh
+++ sembump.sh
@@ -16,4 +16,107 @@
 
 set -eu
 
-echo 'Under Construction...'
+# TODO: Add a flag to treat commits other than Conventional Commits as Patch updates.
+function read_update_type() {
+  while read -r line; do
+    log=$(echo "$line" |
+      sed -nr 's/^(build|chore|ci|docs|feat|fix|perf|refactor|revert|style|test)(\([a-zA-Z_0-9]+\))?(!)?: (.*)$/\1:\3/p')
+    IFS=: read -r commit_type is_breaking <<<"$log"
+    if [ -z "$commit_type" ]; then
+      continue
+    fi
+    if [ "$is_breaking" = '!' ]; then
+      echo 'major'
+    fi
+    if [ "$commit_type" = 'feat' ]; then
+      echo 'minor'
+    else
+      echo 'patch'
+    fi
+  done < <(cat -)
+}
+
+function reduce_update_type() {
+  minor_or_patch=''
+  while read -r update_type; do
+    if [ "$update_type" = 'major' ]; then
+      echo 'major'
+      return 0
+    fi
+    if [ "$update_type" = 'minor' ]; then
+      minor_or_patch='minor'
+    fi
+    if [ "$update_type" = 'patch' ]; then
+      minor_or_patch='patch'
+    fi
+  done < <(cat -)
+  if [ -z $minor_or_patch ]; then
+    return 1
+  fi
+  echo "$minor_or_patch"
+}
+
+function bumpup_version() {
+  current_version="$1"
+  update_type="$2"
+  is_develop="$3"
+  IFS=. read -r major minor patch <<<"$current_version"
+  if [ "$is_develop" = '--develop' ] && [ "$major" != '0' ]; then
+    echo 'The major version of the development version must be 0.' 1>&2
+    return 1
+  fi
+  if [ "$is_develop" != '--develop' ] && [ "$major" = '0' ]; then
+    echo "1.0.0"
+    return 0
+  fi
+  case "$update_type" in
+  "major")
+    if [ "$is_develop" = '--develop' ]; then
+      echo "$major.$((minor + 1)).0"
+    else
+      echo "$((major + 1)).0.0"
+    fi
+    ;;
+  "minor")
+    echo "$major.$((minor + 1)).0"
+    ;;
+  "patch")
+    echo "$major.$minor.$((patch + 1))"
+    ;;
+  *)
+    echo 'Invalid update type was specified.' 1>&2
+    return 1
+    ;;
+  esac
+}
+
+DEVELOP=''
+while (($# > 0)); do
+  case $1 in
+  -d | --develop)
+    if [[ -n "$DEVELOP" ]]; then
+      echo "Duplicated 'option'." 1>&2
+      exit 1
+    fi
+    DEVELOP='--develop'
+    ;;
+  -*)
+    echo "invalid option"
+    exit 1
+    ;;
+  *)
+    CURRENT_VERSION="$1"
+    ;;
+  esac
+  shift
+done
+
+export -f bumpup_version
+if [ -p /dev/stdin ]; then
+  cat -
+else
+  echo "$@"
+fi |
+  read_update_type |
+  reduce_update_type |
+  xargs -I{} bash -c "bumpup_version $CURRENT_VERSION {} $DEVELOP"
